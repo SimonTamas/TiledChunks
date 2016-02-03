0d11e10574d754aefda8a87ed1ab103a2a8d7e90
stamas47 <simontamas47@gmail.com>
2016-02-03 11:26:02 +0100

    Map can now create empty layers

--- LayerData.ts
+++ LayerData.ts
@@ -28,28 +28,28 @@
         public GetChunkLayer(_chunk: TiledChunks.Chunk): TiledChunks.ChunkLayer
         {
             var chunkTileDatas: TiledChunks.TileData[][] = [];
-            var tileRow: number;
-            var tileColumn: number;
-            var id: number;
-            for (var rowOffset = 0; rowOffset < _chunk.map.data.chunkTileRows; rowOffset++)
-            {
-                chunkTileDatas[rowOffset] = [];
-                for (var columnOffset = 0; columnOffset < _chunk.map.data.chunkTileColumns; columnOffset++)
-                {
-                    tileRow = (_chunk.row * _chunk.map.data.chunkTileRows) + rowOffset;
-                    tileColumn = (_chunk.column * _chunk.map.data.chunkTileColumns) + columnOffset;
-                    if (this.tileDatas[tileRow] && this.tileDatas[tileRow][tileColumn] ) {
-                        id = this.tileDatas[tileRow][tileColumn].id;
-                        if (id != this.emptyID)
-                            chunkTileDatas[rowOffset][columnOffset] = new TiledChunks.TileData(id, this.linkedCollisionLayer && this.linkedCollisionLayer.tileDatas[tileRow] && this.linkedCollisionLayer.tileDatas[tileRow][tileColumn] && this.linkedCollisionLayer.tileDatas[tileRow][tileColumn].collides);
-                        else
-                            chunkTileDatas[rowOffset][columnOffset] = null;
-                    }
+            if (this.tileDatas) {
+                var id: number;
+                var tileRow: number;
+                var tileColumn: number;
+                for (var rowOffset = 0; rowOffset < _chunk.map.data.chunkTileRows; rowOffset++) {
+                    chunkTileDatas[rowOffset] = [];
+                    for (var columnOffset = 0; columnOffset < _chunk.map.data.chunkTileColumns; columnOffset++) {
+                        tileRow = (_chunk.row * _chunk.map.data.chunkTileRows) + rowOffset;
+                        tileColumn = (_chunk.column * _chunk.map.data.chunkTileColumns) + columnOffset;
+                        if (this.tileDatas[tileRow] && this.tileDatas[tileRow][tileColumn]) {
+                            id = this.tileDatas[tileRow][tileColumn].id;
+                            if (id != this.emptyID)
+                                chunkTileDatas[rowOffset][columnOffset] = new TiledChunks.TileData(id, this.linkedCollisionLayer && this.linkedCollisionLayer.tileDatas[tileRow] && this.linkedCollisionLayer.tileDatas[tileRow][tileColumn] && this.linkedCollisionLayer.tileDatas[tileRow][tileColumn].collides);
+                            else
+                                chunkTileDatas[rowOffset][columnOffset] = null;
+                        }
 
+                    }
                 }
+                tileRow = null;
+                tileColumn = null;
             }
-            tileRow = null;
-            tileColumn = null;
             return new ChunkLayer(_chunk, this, chunkTileDatas);
         }
 
